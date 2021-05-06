428b91217b9d43b4a59a5abd5875bf2d562109d3
jas473@ored.rutgers.edu <jim.schwanda@rutgers.edu>
2021-05-06 08:36:27 -0400

    remove .bat

--- $ph.bat
+++
@@ -1,2 +0,0 @@
-copy *.js  C:\Users\jas473\Documents\Servers\WAMP\vhosts\projects-health\wp-content\plugins\usi-media-solutions
-copy *.php C:\Users\jas473\Documents\Servers\WAMP\vhosts\projects-health\wp-content\plugins\usi-media-solutions
